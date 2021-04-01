6c6a044bbae8d6d45fd753189e4b9662b2b1daa6
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-04-01 19:43:00 -0400

    assingment coompleted

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = "ishwor";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toLocaleUpperCase() + "!";
+
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
+
